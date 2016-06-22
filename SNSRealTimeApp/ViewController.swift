cfcf10b81a7051d2787117391473ea4c6816e7dd
Hsin <hooy123456@gmail.com>
2016-06-22 11:59:43 +0800

    //it can change the firebase's value with the code self.firebase.setValue("GG")

--- SNSRealTimeApp/ViewController.swift
+++ SNSRealTimeApp/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    let firebase = Firebase(url: "https://snsrealtimeapp-16145.firebaseio.com/")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //當你的Firebase有任何更動時，就可以用這個func去做事情，在這裡我們只是單純的把它print出來
+        //一開始這個function並沒有work，後來把資料庫的read/write都設為true之後便解決了
+        /*Firebase console -> database -> rule
+ {
+ "rules": {
+ ".read": true,
+ ".write": true
+ }
+ }
+ */
+        
+        
+//        firebase.observeEventType(FEventType.Value){(snapshot: FDataSnapshot!)-> Void in
+//            
+//            NSLog("Firebase Observer Calle")
+//            print(snapshot.value)
+//            
+//            self.firebase.setValue("GG")
+//        }
+        
+        //observeEventType: it can do anything u want to do to the firebase
+        firebase.observeEventType(FEventType.Value, withBlock: {(snapshot: FDataSnapshot!) -> Void in
+            NSLog("Firebase Observer Calle")
+            print(snapshot.value)
+            //it can change the firebase's value with the code
+            self.firebase.setValue("GG")
+        })
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
